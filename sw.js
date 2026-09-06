e4063afeb390b662def3933b27d94d62066f6a89
BestGas <53472349+MohamedMahdi2025@users.noreply.github.com>
2026-09-06 16:32:11 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-/* عامل خدمة نظام إدارة الإيجارات — v20260906-787017 */
-var CACHE = "bgc-rental-v20260906-787017";
+/* عامل خدمة نظام إدارة الإيجارات — v20260906-799705 */
+var CACHE = "bgc-rental-v20260906-799705";
 var SHELL = ["./", "./index.html", "./manifest.webmanifest",
   "./icon-192.png", "./icon-512.png", "./icon-maskable-512.png", "./apple-touch-icon.png"];
 
